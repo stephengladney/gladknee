9b2027b259bf0a00554b29a9a77ba444393225ce
Stephen Gladney <stephengladney@gmail.com>
2023-10-01 22:47:18 -0400

    make loremIpsum return longer. default to 69 words

--- gladney.ts
+++ gladney.ts
@@ -1942,7 +1942,9 @@ export function rgbToHex(red: number, green: number, blue: number): string {
  *
  * ```
  */
-export function hexToRgb(hex: string): [number, number, number] {
+export function hexToRgb(
+  hex: string
+): [red: number, green: number, blue: number] {
   const _hex = hex[0] === "#" ? hex.slice(1) : hex
   const redHex = _hex.substring(0, 2)
   const greenHex = _hex.substring(2, 4)
@@ -1971,9 +1973,9 @@ export function hexToRgb(hex: string): [number, number, number] {
  *
  * ```
  */
-export function loremIpsum(wordCount?: number) {
+export function loremIpsum(wordCount = 69) {
   const li =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
   if (wordCount) {
     const words = li.split(" ")
     return words.slice(0, wordCount).join(" ")
