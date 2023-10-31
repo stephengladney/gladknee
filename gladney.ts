0a6fd28085a4071f443cb3bd53843a9d2aa330e3
Stephen Gladney <stephengladney@gmail.com>
2023-10-31 16:41:00 -0400

    October updates (#29)
    * ordinal checks last two digits
    
    * v1.5.3
    
    * add array type to everyNth
    
    * add type inference to removeDuplicates
    
    * add type inference to swapItems
    
    * add object key types to omitKeys
    
    * add result type inference to addPromiseToTimeout
    
    * rm unnecessary reject declaration in pauseAsync
    
    * make loremIpsum return longer. default to 69 words
    
    * add spec for swapItems

--- gladney.ts
+++ gladney.ts
@@ -117,7 +117,8 @@ export function getRange(start: number, end: number, step = 1) {
  * ```
  **/
 export function ordinal(n: number) {
-  if (n >= 11 && n <= 13) return `${String(n)}th`
+  const lastTwoDigits = Number(String(n).slice(-2))
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) return `${String(n)}th`
   switch (String(n).slice(-1)) {
     case "1":
       return `${String(n)}st`
@@ -677,8 +678,8 @@ export function getRandomItem<T>(arr: T[]) {
  * everyNth([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) //=> [3, 6, 9]
  * ```
  **/
-export function everyNth(arr: any[], n: number) {
-  return arr.reduce((acc, item, index) => {
+export function everyNth<T>(arr: T[], n: number) {
+  return arr.reduce((acc: T[], item: T, index: number) => {
     if ((index + 1) % n === 0) return [...acc, item]
     else return acc
   }, [])
@@ -840,7 +841,7 @@ export function insertionSort(arr: StringOrNumberArray) {
  * removeDuplicates([1, 2, 3, 3, 4, 4, 5]) //=> [1, 2, 3, 4, 5]
  * ```
  **/
-export function removeDuplicates(arr: (number | string | object)[]) {
+export function removeDuplicates<T>(arr: T[]): T[] {
   if (typeof arr[0] === "object") {
     const strings = arr.map((obj) => JSON.stringify(obj))
     const uniques = new Set(strings)
@@ -995,7 +996,7 @@ export function getSharedItems<T>(...arrs: T[][]) {
  * ```
  */
 
-export function swapItems(arr: unknown[], index1: number, index2: number) {
+export function swapItems<T>(arr: T[], index1: number, index2: number) {
   return arr.map((item, i) => {
     if (i === index1) return arr[index2]
     if (i === index2) return arr[index1]
@@ -1077,11 +1078,15 @@ export function isObjectInArray(obj: Object, arr: Object[]) {
  * omitKeys(obj, "b") //=> { a: 1, c: 3 }
  * ```
  **/
-export function omitKeys(obj: { [key: string]: any }, ...keys: string[]) {
+export function omitKeys<T extends object, U extends keyof T>(
+  obj: T,
+  ...keys: U[]
+) {
   const result: { [key: string]: any } = {}
+  const keysAsStrings = keys.map((k) => k as string)
   Object.keys(obj).forEach((key: string) => {
-    if (!keys.includes(key)) {
-      result[key as string] = obj[key]
+    if (!keysAsStrings.includes(key)) {
+      result[key as string] = obj[key as U]
     }
   })
   return result
@@ -1564,8 +1569,8 @@ export function convertQueryParamOperators(params: {}) {
 /** Takes a promise and wraps it in another promise that rejects if the original promise takes longer to resolve than a
  * specific amount of time in milliseconds. If the original promise resolves before the timeout, that value is returned.
  **/
-export function addTimeoutToPromise(
-  asyncFunction: () => Promise<unknown>,
+export function addTimeoutToPromise<T>(
+  asyncFunction: () => Promise<T>,
   timeout: number
 ) {
   return () =>
@@ -1578,13 +1583,13 @@ export function addTimeoutToPromise(
       timer = setTimeout(() => {
         reject("TIMED_OUT")
       }, timeout)
-    }) as Promise<unknown>
+    }) as Promise<T>
 }
 
 /** Returns a promise that resolves after a given amount of time in milliseconds.
  **/
 export function pauseAsync(milliseconds: number) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(resolve, milliseconds)
   })
 }
@@ -1937,7 +1942,9 @@ export function rgbToHex(red: number, green: number, blue: number): string {
  *
  * ```
  */
-export function hexToRgb(hex: string): [number, number, number] {
+export function hexToRgb(
+  hex: string
+): [red: number, green: number, blue: number] {
   const _hex = hex[0] === "#" ? hex.slice(1) : hex
   const redHex = _hex.substring(0, 2)
   const greenHex = _hex.substring(2, 4)
@@ -1966,9 +1973,9 @@ export function hexToRgb(hex: string): [number, number, number] {
  *
  * ```
  */
-export function loremIpsum(wordCount?: number) {
+export function loremIpsum(wordCount = 69) {
   const li =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
   if (wordCount) {
     const words = li.split(" ")
     return words.slice(0, wordCount).join(" ")
